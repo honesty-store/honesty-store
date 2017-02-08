b33ec9cea07dea0b262ccb45998996ce988ada4f
Robert Pilling <rpilling@scottlogic.co.uk>
2017-02-08 10:13:13 +0000

    add human error strings for UserError

--- service/src/errorDefinitions.ts
+++ service/src/errorDefinitions.ts
@@ -3,6 +3,11 @@ export type ErrorCode =
   'TooManyPurchaseItems' |
   'UnknownError';
 
+export const humanErrorStrings = {
+  'TopupExceedsMaxBalance': 'Topping up would exceed the maximum balance',
+  'TooManyPurchaseItems': 'Purchase quantity is too large',
+};
+
 export class UserError extends Error {
   public code: number;
 
