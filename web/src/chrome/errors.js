49df1eaa3acb16bbc363078f3dc1b91a0323e9a9
Robert Pilling <rpilling@scottlogic.co.uk>
2017-02-08 16:22:22 +0000

    make error strings more friendly

--- web/src/chrome/errors.js
+++ web/src/chrome/errors.js
@@ -1,7 +1,7 @@
 // this is duplicated in typescript @ service/src/errorDefinitions.ts
 
 export default {
-  TopupExceedsMaxBalance: { humanReadable: 'Topping up would exceed the maximum balance', retryable: false },
-  TooManyPurchaseItems: { humanReadable: 'Purchase quantity is too large', retryable: false },
-  EmailNotFound: { humanReadable: 'Email not found', retryable: true }
+  TopupExceedsMaxBalance: { humanReadable: 'Topping up would exceed your maximum balance', retryable: false },
+  TooManyPurchaseItems: { humanReadable: 'You\'re purchasing too many items', retryable: false },
+  EmailNotFound: { humanReadable: 'Couldn\'t find your email', retryable: true } // this should be handled transparently and never been seen
 };
