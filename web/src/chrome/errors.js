2f991c4badaf95da9945352a8d2a8fe0fbd0ebd7
Robert Pilling <rpilling@scottlogic.co.uk>
2017-02-08 14:29:13 +0000

    add note about duplicate error definitions

--- web/src/chrome/errors.js
+++ web/src/chrome/errors.js
@@ -1,3 +1,5 @@
+// this is duplicated in typescript @ service/src/errorDefinitions.ts
+
 export default {
   TopupExceedsMaxBalance: 'Topping up would exceed the maximum balance',
   TooManyPurchaseItems: 'Purchase quantity is too large',
