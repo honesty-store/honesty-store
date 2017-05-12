30d5f803a2efaaa6d27f32812627a3fef295c9e9
Robert Pilling <rpilling@scottlogic.co.uk>
2017-05-12 14:59:27 +0100

    add lambda to web

--- aws/src/script/deploy.ts
+++ aws/src/script/deploy.ts
@@ -63,6 +63,7 @@ const lambdaConfig = {
   },
   web: {
     database: 'none',
+    handler: 'lambda.handler',
     catchAllResource: true
   }
 };
@@ -226,7 +227,7 @@ export default async ({ branch, dirs }) => {
       name: generateName({ branch, dir }),
       codeDirectory: dir,
       codeFilter: path => /(lib|node_modules)/.test(path),
-      handler: `lib/${dir}/src/lambda.handler`,
+      handler: lambdaConfig[dir].handler || `lib/${dir}/src/lambda.handler`,
       dynamoAccess: lambdaConfig[dir].database,
       withApiGateway: true,
       live: isLive(branch),
