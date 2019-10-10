2ada4c598bf5e6079bf85f8cd81e045db7d61f1c
Zac Rubin <z-0@users.noreply.github.com>
2019-10-10 14:27:12 +0100

    cdk - initial package & CDK config

---
+++ cdk/bin/honesty-store.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { HonestyStore } from '../lib/honesty-store';
+
+const app = new cdk.App();
+new HonestyStore(app, 'honesty-store');
